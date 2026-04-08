ee723fcdce59fc1f4cbc386bce8601e2d708fbd2
Peter.Chen <tigernaxo@gmail.com>
2026-04-08 22:57:52 +0800

    ..

--- MyOpenAI.cs
+++ MyOpenAI.cs
@@ -20,15 +20,21 @@ namespace MyAgentFramework
             OpenAIClient client = new OpenAIClient(new ApiKeyCredential("apikey"), new OpenAIClientOptions() { 
                 Endpoint = new Uri("http://192.168.68.250:11434/v1/")
             });
-            var chatClient = client.GetChatClient("gemma4:26B");
+            var chatClient = client.GetChatClient("gemma4:26B-128k");
 
             AIAgent agent = chatClient.AsAIAgent(
                 instructions: "You are a helpful assistant.  Keep your answers brief.",
                 name: "Joker",
                 tools: [AIFunctionFactory.Create(Weather.GetWeather)]
                 );
+            Microsoft.Extensions.AI.ChatMessage chatMessage = new(
+                ChatRole.User, [
+                new TextContent("What do you see in this image? 請以繁體中文回答"),
+                new DataContent("data:image/jpeg;base64,/9j/4gIcSUNDX1BST0ZJTEU...=")
+            ]);
 
-            return await agent.RunAsync("Tell me weather about 龍井.");
+            //return await agent.RunAsync("Tell me weather about 龍井.");
+            return await agent.RunAsync(chatMessage);
         }
     }
 }
